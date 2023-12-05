b247324368595f3d057735b5ef06ff78989d1ff8
Gustavo Schnurr Siqueira <311100469@senacinformatica.br>
2023-12-05 20:48:59 -0300

    Primeiro commit do validador de cpf

---
+++ src/geracpf/GeraCPF.java
@@ -0,0 +1,21 @@
+
+package geracpf;
+
+import javax.swing.JFrame;
+
+
+public class GeraCPF {
+
+
+    public static void main(String[] args) {
+        
+        TelaTeste janela1 = new TelaTeste ();
+        janela1.setSize(380, 240);
+        janela1.setVisible(true);
+        janela1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        
+    }
+    
+}
